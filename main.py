e70ddf252e99f87782db241bac83c37985c3bf35
Jekyll-007 <teenofeejay@gmail.com>
2024-10-11 17:46:45 +0100

    date time

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-numb1=int(input("insert a number: "))
-if numb1%2==0:
-    print("number is even")
-else: 
-    print("number is odd")    
+#numb1=int(input("insert a number: "))
+#if numb1%2==0:
+ #   print("number is even")
+#else: 
+ #   print("number is odd")
+
+#  Date and Time Operation
+import datetime
+
+current_time=datetime.datetime.now()
+print("The time in GMT is: ", current_time)
+
+import calendar
+print("\n", calendar.calendar(2020))
